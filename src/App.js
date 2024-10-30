ee3ca09135397c85794521fef935548a45a6d819
추성우 <cntjddn715@gmail.com>
2024-10-30 10:58:31 +0900

    chore :: 메뉴바 아이콘 색상

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/main";
+import Brand from "./pages/brand";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/brand" element={<Main />} />
+      <Route path="/brand" element={<Brand />} />
     </Routes>
   );
 }
--- src/pages/brand/index.jsx
+++ src/pages/brand/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import * as _ from "./style";
+import Header from "../../components/Header";
+import MenuBar from "../../components/MenuBar";
 
 const Brand = () => {
-  return <_.Content>
-    
-  </_.Content>;
+  return (
+    <_.Layout>
+      <Header />
+      <MenuBar />
+    </_.Layout>
+  );
 };
 
 export default Brand;
